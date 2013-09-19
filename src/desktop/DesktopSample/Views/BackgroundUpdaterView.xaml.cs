6cb957be954765a3a8e2a377f815ab50986c6000
Brendan Forster <brendan@github.com>
2013-09-19 19:22:22 +0930

    fleshing out the other views

---
+++ src/desktop/DesktopSample/Views/BackgroundUpdaterView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using ReactiveUI;
+using Shimmer.DesktopDemo.ViewModels;
+
+namespace Shimmer.DesktopDemo.Views
+{
+    public partial class BackgroundUpdaterView : IViewFor<BackgroundUpdaterViewModel>
+    {
+        public BackgroundUpdaterView()
+        {
+            InitializeComponent();
+        }
+
+        public BackgroundUpdaterViewModel ViewModel
+        {
+            get { return (BackgroundUpdaterViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(BackgroundUpdaterViewModel), typeof(BackgroundUpdaterView), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (BackgroundUpdaterViewModel)value; }
+        }
+    }
+}
---
+++ src/desktop/DesktopSample/Views/ForegroundUpdaterView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using ReactiveUI;
+using Shimmer.DesktopDemo.ViewModels;
+
+namespace Shimmer.DesktopDemo.Views
+{
+    public partial class ForegroundUpdaterView : IViewFor<ForegroundUpdaterViewModel>
+    {
+        public ForegroundUpdaterView()
+        {
+            InitializeComponent();
+        }
+
+        public ForegroundUpdaterViewModel ViewModel
+        {
+            get { return (ForegroundUpdaterViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(ForegroundUpdaterViewModel), typeof(ForegroundUpdaterView), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (ForegroundUpdaterViewModel)value; }
+        }
+    }
+}
